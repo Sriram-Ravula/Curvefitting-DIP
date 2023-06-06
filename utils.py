81167f2c16934828b448641e04e79bb9a011bbb4
Sriram Ravula <sravula@login2.ls6.tacc.utexas.edu>
2023-06-06 13:55:07 -0500

    Updating utils anf Fitting - untested

--- utils.py
+++ utils.py
@@ -1,22 +1,8 @@
 import torch
 import torch.nn as nn
-import random
 import numpy as np
 import os
-import skrf
 import matplotlib.pyplot as plt
-
-def set_all_seeds(seed):
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    # Torch RNG
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    # Python RNG
-    np.random.seed(seed)
-    random.seed(seed)
-
-    return
 
 def get_inds(problem_type, length, num_kept_samples):
     """
@@ -151,119 +137,25 @@ def get_network_from_file(root_pth, chip_num):
     
     return out_dict
 
-def network_to_sparams(network, reciprocity_eps = 1e-6):
+def network_to_sparams(network):
     """
     Converts a given Sk-RF network object into a [1, 2*N_-sparams, N_freqs] torch tensor.
-    Checks to see if the network is reciprocal - if true, N_sparams will be the number of UNIQUE S-params,
-        not total.
     
     Args:
         network: sk-rf network object.
-        reciprocity_eps: the tolerance for determining if the network is reciprocal
     
     Returns:
         [1, 2*N_sparams, N_freqs] torch tensor. Channel dimenson has reals
             on the even indices and their respective imag's on the odd indices. 
     """
-    #(1) check if the network is reciprocal
-    # reciprocity = skrf.network.reciprocity(network.s)
-
-    # IS_RECIPROCAL = False
-    # if np.sum(np.abs(reciprocity)) < reciprocity_eps:
-    #     IS_RECIPROCAL = True
-    
-    #(2) convert the (N_FREQS, N_PORTS, N_PORTS) complex s-param matrix to 
-    # (N_FREQS, N_PORTS, N_PORTS, RE/IM) real-valued matrix
     re_mat = network.s.real
     im_mat = network.s.imag
     out_mat = np.stack((re_mat, im_mat), axis=-1)
 
-    # #(3) grab the unique or all s-parameters and return
-    # if IS_RECIPROCAL:
-    #     out_sparams = matrix_to_sparams(out_mat)
-
-    #     return torch.from_numpy(out_sparams).view(-1, out_sparams.shape[-1]).unsqueeze(0).type(torch.float32)
-    # else:
     out_mat = out_mat.reshape((out_mat.shape[0], -1, out_mat.shape[-1])) #[FREQ, N_PORT^2, RE/IM]
     out_mat = out_mat.reshape((out_mat.shape[0], -1)).transpose() #[Re/im Sparams, FREQ]
     
     return torch.from_numpy(out_mat).unsqueeze(0).type(torch.float32)
-
-def grab_chip_data(root_pth, chip_num):
-    """
-    Given a root path and a chip number, grab all the relevant info for a chip.
-    Converts the data type of the output to float32.
-    
-    Args:
-        root_pth: Root of the folder containing chip info.
-        chip_num: Chip number.
-    
-    Returns:
-        out_dict: Dictionary with all relevant chip data.
-                  Entries:
-                      "gt_freqs": Ground truth frequencies.
-                                  (1D numpy array).
-                      "gt_matrix": Ground truth S-param matrix. 
-                                  (4D numpy array) - Axes are [Freq, In_port, Out_port, Re/Im].
-                      "vf_matrix": VF interpolation given samples.
-                                   (4D numpy array).
-                      "y_freqs": Observed sample frequencies chosen by VF.
-                                 (1D numpy array).
-                      "y_matrix": Ground truth matrix sampled at the observed frequencies.
-                                  (4D numpy array).
-    """
-    from skrf import Network
-    
-    #Grab the correct folder
-    chip_num = str(chip_num) if chip_num > 9 else "0" + str(chip_num)
-    fname = os.path.join(root_pth, "case"+chip_num)
-    
-    def grab_network_info(folder_pth, net_str):
-        """
-        Helper function that takes a string, searches a given folder for touchstone 
-            files matching the string, grabs the S-param and frequency data, and 
-            formats and returns the S-param matrix and the corresponding frequencies.
-        """
-        
-        #grab the correct file we want
-        children = os.listdir(folder_pth)
-        children = [f for f in children if net_str in f]
-        
-        #there should only be a single file with the given string
-        if len(children) > 1:
-             return None, None
-        else:
-            children = children[0]
-        
-        #grab the actual network stuff now
-        data_path = os.path.join(folder_pth, children)
-        
-        out_network = Network(data_path)
-        
-        out_matrix_re = out_network.s.real
-        out_matrix_im = out_network.s.imag
-        out_matrix = np.stack((out_matrix_re, out_matrix_im), axis=-1)
-
-        out_freqs = out_network.f.squeeze()
-        
-        return out_matrix, out_freqs
-    
-    #now make the proper filename strings and grab the gt, VF, and y data
-    gt_str = str(chip_num) + ".s"
-    vf_str = str(chip_num) + ".HLAS.s"
-    y_str = "SIEMENS_AFS_SAMPLE_POINT_SIMULATIONS.s"
-
-    gt_matrix, gt_freqs = grab_network_info(fname, gt_str)    
-    vf_matrix, _ = grab_network_info(fname, vf_str)
-    y_matrix, y_freqs = grab_network_info(fname, y_str)
-
-    out_dict = {"gt_matrix": gt_matrix.astype('float32'),
-                "gt_freqs": gt_freqs,
-                "vf_matrix": vf_matrix.astype('float32'),
-                "y_matrix": y_matrix.astype('float32'),
-                "y_freqs": y_freqs}
-            
-    return out_dict
 
 def matrix_to_network(data_matrix, data_freqs, name, resample_freqs=None):
     """
@@ -303,69 +195,6 @@ def matrix_to_network(data_matrix, data_freqs, name, resample_freqs=None):
 
     return net
 
-def matrix_to_sparams(data_matrix):
-    """
-    Takes a raw 4D sparam matrix and returns a 3D array of sparam series.
-    Output is float32.
-
-    Args:
-        data_matrix: Raw 4D sparam matrix. 
-                     (4D numpy array) - Axes are [Freq, In_port, Out_port, Re/Im].
-
-    Returns:
-        output: 3D array of time series.
-                (3D numpy array) - [Unique Port Pair, Re/Im, Freq].
-    """
-    num_freqs = data_matrix.shape[0]
-    num_ports = data_matrix.shape[1]
-
-    num_unique = int(num_ports * (num_ports + 1) / 2)
-    
-    output = np.zeros((num_unique, 2, num_freqs), dtype='float32')
-
-    t = 0
-    for i in range(num_ports):
-        for j in range(i+1):
-            output[t] = np.copy(data_matrix[:, i, j, :]).transpose()
-            t += 1
-
-    return output
-
-def sparams_to_matrix(sparams_data):
-    """
-    Function for converting a 3-D frequency series sparam data back to 
-        a 4-D frequency matrix series.
-    Output is the same datatype as the input.
-    
-    Args:
-        sparams_data: 3D frequnecy series.
-                      Torch tensor [1, 2 * unique_sparams, Freq].
-    
-    Returns:
-        A: 4D matrix sparam frequency series. 
-           Torch tensor [Freq, In_port, Out_port, Re/Im].
-    """
-    _, num_unique, num_freqs = sparams_data.shape
-    num_unique = num_unique // 2
-
-    num_ports = (-1 + np.sqrt(8*num_unique + 1)) // 2
-    num_ports = int(num_ports)
-
-    A = torch.zeros(num_freqs, num_ports, num_ports, 2).to(sparams_data.device).type(sparams_data.dtype)
-
-    for i in range(num_ports):
-        for j in range(i+1):
-            A[:, i, j, 0] = sparams_data[0, i*(i+1) + 2*j, :].clone()
-            A[:, i, j, 1] = sparams_data[0, i*(i+1) + 2*j + 1, :].clone()
-    
-    out_mat = A.clone()
-
-    torch.diagonal(A, 0, 1, 2).zero_()
-
-    out_mat += A.transpose(1, 2)
-
-    return out_mat
-
 def sparams_to_mag(sparams, get_phase=False, in_db=False):
     """
     Computes the magnitude, and optionally the phase, of a given complex signal.
@@ -396,73 +225,6 @@ def sparams_to_mag(sparams, get_phase=False, in_db=False):
         return x_mag, x_phase
     else:
         return x_mag
-
-#NOTE deprecated old method
-def to_mag(data):
-    """
-    Converts a given signal in re/im to magnitude in Db.
-
-    Args:
-        data: A time/frequency series signal with real and imaginary channels.
-              Can have shape [1, 2*num_sparams, length] where the second channel
-                holds the real and imaginary parts of each s-param.
-              Can also have shape [num_sparams, 2, length].
-              Torch Tensor.
-    
-    Returns:
-        magnitude_data: magnitude of each unique s-parameter in decibels.
-    """
-    if data.shape[0] == 1:
-        n_sparams = data.shape[1] // 2
-        data = data.squeeze().view(n_sparams, 2, -1)
-    
-    n_sparams = data.shape[0]
-    length = data.shape[-1]
-    
-    mag_data = torch.zeros(n_sparams, length)
-    
-    for i in range(n_sparams):
-        mag_data[i] = torch.square(data[i, 0, :]) + torch.square(data[i, 1, :])
-    
-    return 10 * torch.log10(mag_data)
-
-def sparams_to_sing_vals(sparams_data):
-    """
-    Function for taking 3-D frequency series sparam data and
-        calculating the singular values for every frequency.
-
-    Args:
-        sparams_data: 3D frequnecy series.
-                      Torch tensor [1, 2 * unique_sparams, Freq].
-    
-    Returns:
-        svd: The SVDs of each (num_ports x num_ports) matrix in the frequency series.
-             Torch tensor [num_freqs, num_ports]
-    """
-    _, num_unique, num_freqs = sparams_data.shape
-    num_unique = num_unique // 2
-
-    num_ports = (-1 + np.sqrt(8*num_unique + 1)) // 2
-    num_ports = int(num_ports)
-
-    A = torch.zeros(num_freqs, num_ports, num_ports, 2, device=sparams_data.device, dtype=sparams_data.dtype)
-
-    for i in range(num_ports):
-        for j in range(i+1):
-            #grab the real and imaginary parts
-            #if they're diagonal, divide by two to account for double addition when we sum transpose
-            if i != j:
-                A[:, i, j, 0] = sparams_data[0, i*(i+1) + 2*j, :]
-                A[:, i, j, 1] = sparams_data[0, i*(i+1) + 2*j + 1, :]
-            else:
-                A[:, i, j, 0] = sparams_data[0, i*(i+1) + 2*j, :] / 2
-                A[:, i, j, 1] = sparams_data[0, i*(i+1) + 2*j + 1, :] / 2
-    
-    A = A + A.transpose(1, 2)
-
-    #torch.complex.... makes a [num_freqs, num_ports, num_ports] tensor
-    #must have compute_UV = True to use gradients
-    return torch.linalg.svd(torch.complex(A[..., 0], A[..., 1]))[1]
 
 def plot_mags(sparams_list, freqs=None, chip_labels=None, not_reciprocal=False, plot_subset_list=None, ylims=False):
     mag_list = [sparams_to_mag(sparam) for sparam in sparams_list]
@@ -588,4 +350,3 @@ class Smoothing_Loss(nn.Module):
                 rloss_per_chan = torch.sqrt(loss_per_chan) #[1, 2 * N_sparams]
                 
                 return torch.sum(rloss_per_chan) 
-                
